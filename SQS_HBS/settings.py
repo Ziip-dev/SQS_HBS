a8351a746f1b5d6fc738e26055723d6bade63ba7
Ziip <paul.dulaud@utt.fr>
2022-05-16 18:11:50 +0200

    Small changes to dashboard app

--- SQS_HBS/settings.py
+++ SQS_HBS/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'dashboard',
+    'dashboard.apps.DashboardConfig',
     'fitapp',
     'django_simple_bulma',
     'widget_tweaks',

59f10f7794a3ab332d1bc585f3fa33481510f535
Ziip <paul.dulaud@utt.fr>
2022-06-21 19:29:10 +0200

    Integrate data in template

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,12 +2,23 @@
 
 from django.shortcuts import redirect, render
 from fitapp.decorators import fitbit_integration_warning
+from fitapp.models import TimeSeriesData
 
 
 @fitbit_integration_warning(msg="Integrate your account with Fitbit!")
 def home(request):
     if request.user.is_authenticated:
-        return render(request, "dashboard/home.html")
+        # retrieve user data from database
+        # now = datetime.datetime.now()  # parse to "YYYY-MM-DD"
+        tsds = TimeSeriesData.objects.filter(user=request.user, date="2022-06-20")
+
+        # for tsd in TimeSeriesData.objects.filter(user=self.user, date=date):
+
+        # TimeSeriesData.objects.filter(user=self.user, date=date).count(),
+
+        # activities = TimeSeriesDataType.activities
+
+        return render(request, "dashboard/home.html", {"tsds": tsds})
 
     else:
         return redirect("login")

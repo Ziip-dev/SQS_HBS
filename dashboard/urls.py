4efebfcf2b6e9bf4524035e2a946e88604ce14b8
Ziip <paul.dulaud@utt.fr>
2022-07-08 18:48:51 +0200

    Add offline capabilities

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
+from django.views.generic.base import TemplateView
 
 from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("sw.js", views.service_worker),
+    path(
+        "offline/",
+        TemplateView.as_view(template_name="dashboard/offline.html"),
+        name="offline",
+    ),
 ]

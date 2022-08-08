1146d298f35631358b31636bf0cf315b91d4ef60
Ziip <paul.dulaud@utt.fr>
2022-08-08 11:36:25 +0200

    Add StartDate model

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from fitapp.models import UserModel
 
-# Create your models here.
+
+class StartDate(models.Model):
+    """
+    The StartDate model stores a new date for the user's start date.
+
+    :param auto_now_add=True: Automatically assigns the value of the field to
+                              the current date when the object is first
+                              recorded.
+    :return: The date the user joined.
+    """
+
+    user = models.OneToOneField(
+        UserModel,
+        primary_key=True,
+        on_delete=models.CASCADE,
+        help_text="The current user",
+    )
+
+    start_date = models.DateField(
+        auto_now_add=True, help_text="The date the user started using the app."
+    )
+
+    def __str__(self):
+        return self.start_date.strftime("%Y-%m-%d")

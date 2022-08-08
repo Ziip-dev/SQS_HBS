43a44c8eff534bd0379583e3018f31472584c90b
Ziip <paul.dulaud@utt.fr>
2022-08-08 11:35:02 +0200

    Refactor login template

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from django.views.generic.base import TemplateView
 
@@ -5,7 +6,12 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("sw.js", views.service_worker),
+    path(
+        "login/",
+        auth_views.LoginView.as_view(template_name="dashboard/login.html"),
+        name="login",
+    ),
+    path("sw.js/", views.service_worker),
     path(
         "offline/",
         TemplateView.as_view(template_name="dashboard/offline.html"),

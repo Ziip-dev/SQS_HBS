d967f3511153822aae81b3a9eb989f13aa63f94a
Ziip <paul.dulaud@utt.fr>
2022-08-08 11:36:59 +0200

    Update the home view to manage the start date

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,6 @@
 # dashboard/views.py
 
+from datetime import date, datetime, timedelta
 from pathlib import Path
 
 from django.conf import settings
@@ -8,21 +9,51 @@ from django.shortcuts import redirect, render
 from fitapp.decorators import fitbit_integration_warning
 from fitapp.models import TimeSeriesData
 
+from .models import StartDate
 
-@fitbit_integration_warning(msg="Integrate your account with Fitbit!")
+from birdseye import eye
+
+
+@fitbit_integration_warning(
+    msg="Connectez-vous à Fitbit à l'aide des identifiants fournis"
+)
+@eye
 def home(request):
     if request.user.is_authenticated:
-        # retrieve user data from database
-        # now = datetime.datetime.now()  # parse to "YYYY-MM-DD"
-        tsds = TimeSeriesData.objects.filter(user=request.user, date="2022-06-29")
+        #  If not already present, store the experiment start date after the first login
+        if not StartDate.objects.filter(user=request.user).exists():
+            StartDate(user=request.user).save()
 
-        # for tsd in TimeSeriesData.objects.filter(user=self.user, date=date):
+        # Retrieve the necessary data for the 2-week steam graph
+        # start date as datetime object and current day
+        start_date = StartDate.objects.get(user=request.user).start_date
+        # today = date.today()
 
-        # TimeSeriesData.objects.filter(user=self.user, date=date).count(),
+        # current week
+        # current_week = date - timedelta(days=7)
 
-        # activities = TimeSeriesDataType.activities
+        # last week
+        # last_week =
 
-        return render(request, "dashboard/home.html", {"tsds": tsds})
+        tsds = TimeSeriesData.objects.filter(
+            user=request.user, date__gte=start_date).order_by("date")
+
+        print("")
+        for tsd in tsds:
+            print(
+                f"    - {tsd.date}: {tsd.user}, {tsd.value} {tsd.resource_type.resource}")
+        print("")
+
+        # TODO ok j'ai toute la récupération qui marche bien, plus qu'à parser :D
+        # On somme minutesFairlyActive et minutesVeryActive pour avoir
+        # le niveau d'AP pour un jour, qu'on passe au template.
+
+        # FIXME pas besoin de passer tout le QuerySet, c'était pour tester.
+        context = {
+            "tsds": tsds,
+        }
+
+        return render(request, "dashboard/home.html", context)
 
     else:
         return redirect("login")
@@ -30,5 +61,6 @@ def home(request):
 
 def service_worker(request):
     sw_path = Path(settings.STATIC_ROOT, "sw.js")
-    response = HttpResponse(open(sw_path).read(), content_type="application/javascript")
+    response = HttpResponse(open(sw_path).read(),
+                            content_type="application/javascript")
     return response

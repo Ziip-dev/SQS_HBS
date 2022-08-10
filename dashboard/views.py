609c4f8788917cfbded837aeb3bfa4dbddca58d8
Ziip <paul.dulaud@utt.fr>
2022-08-10 12:52:25 +0200

    Update views.py - data parsing
    - Retrieve data from database
    - Aggregate data by day
    - Create weekly lists with PA values
    - Feed context dictionary to pass to the template

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,17 +1,17 @@
 # dashboard/views.py
 
-from datetime import date, datetime, timedelta
 from pathlib import Path
 
+from birdseye import eye
 from django.conf import settings
+from django.db.models import IntegerField, Sum
+from django.db.models.functions import Cast
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from fitapp.decorators import fitbit_integration_warning
 from fitapp.models import TimeSeriesData
 
 from .models import StartDate
-
-from birdseye import eye
 
 
 @fitbit_integration_warning(
@@ -24,33 +24,59 @@ def home(request):
         if not StartDate.objects.filter(user=request.user).exists():
             StartDate(user=request.user).save()
 
-        # Retrieve the necessary data for the 2-week steam graph
-        # start date as datetime object and current day
-        start_date = StartDate.objects.get(user=request.user).start_date
-        # today = date.today()
+        # Retrieve and parse user data for the 2-week steam graph
+        # start date as datetime object
+        from datetime import timedelta
 
-        # current week
-        # current_week = date - timedelta(days=7)
+        start_date = StartDate.objects.get(user=request.user).start_date - timedelta(
+            days=13
+        )
 
-        # last week
-        # last_week =
+        # retrieve necessary user's physical activity (PA) data from database
+        wanted_resources = ["minutesFairlyActive", "minutesVeryActive"]
+        user_PA_data = TimeSeriesData.objects.filter(
+            user=request.user,
+            resource_type__resource__in=wanted_resources,
+            date__gte=start_date,
+        )
 
-        tsds = TimeSeriesData.objects.filter(
-            user=request.user, date__gte=start_date).order_by("date")
+        # sum minutesFairlyActive and minutesVeryActive values per day
+        aggregated_PA_data = (
+            user_PA_data.values("date")
+            .annotate(minutesActive=Sum(Cast("value", IntegerField())))
+            .order_by("date")
+        )
 
-        print("")
-        for tsd in tsds:
-            print(
-                f"    - {tsd.date}: {tsd.user}, {tsd.value} {tsd.resource_type.resource}")
-        print("")
+        # separate PA data into lists of 7 days
+        weekly_PA_data = [
+            aggregated_PA_data[d: d + 7]
+            for d in range(0, aggregated_PA_data.count(), 7)
+        ]
 
-        # TODO ok j'ai toute la récupération qui marche bien, plus qu'à parser :D
-        # On somme minutesFairlyActive et minutesVeryActive pour avoir
-        # le niveau d'AP pour un jour, qu'on passe au template.
+        # retrieve the first two weeks of the experiment
+        week_1_PA_data = weekly_PA_data[0]
+        # TODO là va y avoir un soucis d'OOB pendant la première semaine d'XP...
+        week_2_PA_data = weekly_PA_data[1]
 
-        # FIXME pas besoin de passer tout le QuerySet, c'était pour tester.
+        # isolate PA values as lists, and complete with 0 for the remaining days of the week
+        week_1_PA_values = [d.get("minutesActive") for d in week_1_PA_data] + [0] * (
+            7 - week_1_PA_data.count()
+        )
+        week_2_PA_values = [d.get("minutesActive") for d in week_2_PA_data] + [0] * (
+            7 - week_2_PA_data.count()
+        )
+
+        print(
+            f"""
+              {week_1_PA_values}
+              {week_2_PA_values}
+              """
+        )
+
+        # create context dictionary with one entry per week
         context = {
-            "tsds": tsds,
+            "PA_1": week_1_PA_values,
+            "PA_2": week_2_PA_values,
         }
 
         return render(request, "dashboard/home.html", context)

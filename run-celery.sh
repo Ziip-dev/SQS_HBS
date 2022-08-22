5b5642cb77edcf171f230100791eb1c52442e1f9
Ziip <paul.dulaud@utt.fr>
2022-08-22 12:30:54 +0200

    Update celery run script

--- run-celery.sh
+++ run-celery.sh
@@ -5,10 +5,6 @@ set -eu
 # Activate virtual environment
 source /home/ziip/pysetup/.venv/bin/activate
 
-# Update webserver - migrations and staticfiles
-python manage.py collectstatic --noinput
-python manage.py migrate
-
 # Run celery worker
 celery --app=SQS_HBS worker --loglevel=WARNING --pool=eventlet --concurrency=100
 

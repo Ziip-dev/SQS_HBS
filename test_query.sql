eb0f77133424b6142ef0ac627339435e8477fa92
Ziip <paul.dulaud@utt.fr>
2022-09-04 18:02:51 +0200

    Update SQL test_query

--- test_query.sql
+++ test_query.sql
@@ -2,7 +2,6 @@
 -- @group SQS_HBS
 -- @name test update continu
 
-
 SELECT fitapp_timeseriesdata.date,
        auth_user.username,
        fitapp_timeseriesdatatype.resource,
@@ -12,7 +11,7 @@ FROM fitapp_timeseriesdata
 JOIN auth_user ON fitapp_timeseriesdata.user_id=auth_user.id
 JOIN fitapp_timeseriesdatatype ON fitapp_timeseriesdata.resource_type_id=fitapp_timeseriesdatatype.id
 
-WHERE fitapp_timeseriesdata.date >= '2022-08-07'
+WHERE fitapp_timeseriesdata.date >= '2022-08-20'
 AND (fitapp_timeseriesdatatype.resource = 'minutesFairlyActive'
   OR fitapp_timeseriesdatatype.resource = 'minutesVeryActive')
 

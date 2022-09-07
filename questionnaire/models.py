39495d71855879166d9225eeee26eaa93bb913d8
Ziip <paul.dulaud@utt.fr>
2022-09-07 14:35:38 +0200

    Add models and populate questions

--- questionnaire/models.py
+++ questionnaire/models.py
@@ -1,17 +1,26 @@
 from django.db import models
+from django.urls import reverse
 from fitapp.models import UserModel
 
 
 class Questions(models.Model):
-    # 1 table avec une question par ligne identifiée par son numéro en pk
-    question = models.TextField
+    question = models.TextField("Question")
+
+    def __str__(self):
+        return self.question
 
 
 class Answers(models.Model):
+    user = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+    question = models.ForeignKey(Questions, on_delete=models.CASCADE)
+    answer = models.IntegerField()
+    created = models.DateField(auto_now_add=True)
+    updated = models.DateField(auto_now=True)
 
-    user = models.OneToOneField(
-        UserModel,
-        primary_key=True,
-        on_delete=models.CASCADE,
-        help_text="The current user",
-    )
+    def get_absolute_url(self):
+        return reverse("answer-update", kwargs={"pk": self.pk})
+
+    def __str__(self):
+        return (
+            f"User {self.user} has answered {self.answer} to question {self.question}."
+        )

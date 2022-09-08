fd4d1f36957d4bb1dfce6cc197da8e5ad66d9ae3
Ziip <paul.dulaud@utt.fr>
2022-09-09 00:46:05 +0200

    Working CreateView with ModelForm

--- questionnaire/views.py
+++ questionnaire/views.py
@@ -1,61 +1,41 @@
-# from django.shortcuts import get_object_or_404, redirect, render
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 
 from .forms import QuestionnaireForm
-from .models import Answer, Questionnaire
-
-# from fitapp.utils import is_integrated
+from .models import Answer, Question, Questionnaire
 
 
-# class QuestionnaireView(FormView):
-#     template_name = "questionnaire/questionnaire.html"
-#     form_class = Questionnaire
-#     success_url = reverse_lazy("answer-add")
-
-#     def form_valid(self, form):
-#         # form.save()
-#         return super().form_valid(form)
-
-# def get(self, request, pk):
-#     if request.user.is_authenticated and is_integrated(request.user):
-
-#         question = get_object_or_404(Questions, pk=pk)
-
-#         # Create context dictionary for the template
-#         context = {
-#             "question": question,
-#         }
-
-#         return render(request, self.template_name, context)
-
-#     else:
-#         redirect("login")
-
-# def post(self, request):
-#     pass
-
-
-# class QuestionnaireView(CreateView):
-#     template_name = "questionnaire/questionnaire.html"
-#     success_url = reverse_lazy("questionnaire")
-#     # fields = ["question", "answer"]
-#     # context_object_name = "answers"
-
-#     def get_success_url(self, **kwargs):
-#         pass
-
-
-# ANTOINE
-class QuestionnaireView(CreateView):
+class QuestionnaireView(LoginRequiredMixin, CreateView):
     model = Answer
     form_class = QuestionnaireForm
+    template_name = "questionnaire/questionnaire.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        questionnaire = Questionnaire.get(pk=kwargs["questionaire_pk"])
-
-        # django fait de la magie et fout un question_set dans le questionnaire si y'a des foreign key Question->Questionnaire
-        # https://docs.djangoproject.com/en/4.1/topics/db/examples/many_to_one/
-        context["question"] = questionnaire.question_set[kwargs["question"]]
+        questionnaire = Questionnaire.objects.get(pk=self.kwargs["questionnaire_pk"])
+        context["question"] = questionnaire.question_set.all()[self.kwargs["question"]]
         return context
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        form.instance.question = Question.objects.get(pk=self.kwargs["question"] + 1)
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        question_id = self.kwargs["question"] + 1
+        questionnaire_pk = self.kwargs["questionnaire_pk"]
+        max_question = Question.objects.filter(
+            questionnaire_id=questionnaire_pk
+        ).count()
+
+        # Return to questionnaire with the next question
+        if question_id < max_question:
+            return reverse_lazy(
+                "questionnaire",
+                kwargs={"questionnaire_pk": questionnaire_pk, "question": question_id},
+            )
+
+        # Return to home when all the questions have been answered
+        elif question_id == max_question:
+            return reverse_lazy("home")

42a2c5ba9e5327ac2c0d3a7af99013178f1cb9f7
Ziip <paul.dulaud@utt.fr>
2022-09-09 00:41:37 +0200

    models + migrations + fixtures

--- questionnaire/models.py
+++ questionnaire/models.py
@@ -1,17 +1,17 @@
 from django.db import models
-from django.urls import reverse
 from fitapp.models import UserModel
 
-# class Questions(models.Model):
-#     question = models.TextField("Question")
 
-
-# ANTOINE
 class Questionnaire(models.Model):
-    questionaire_pk = models.IntegerField()
+    """
+    Model containing every questions.
+    Linked by the Question.questionnaire ForeignKey in each question.
+    """
+
+    questionnaire_pk = models.IntegerField()
 
     def __str__(self):
-        return self.questionaire_pk
+        return str(self.questionnaire_pk)
 
 
 class Question(models.Model):
@@ -27,7 +27,6 @@ class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     answer = models.IntegerField()
     created = models.DateField(auto_now_add=True)
-    updated = models.DateField(auto_now=True)
 
     def __str__(self):
-        return self.answer
+        return f"{self.user} - question {self.question.id} ({self.created})"

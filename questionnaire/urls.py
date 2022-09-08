61058a436783e619e0af478ffb45a8b2f0307c6e
Ziip <paul.dulaud@utt.fr>
2022-09-09 00:45:27 +0200

    Adjust urls

--- questionnaire/urls.py
+++ questionnaire/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from django.views.generic.base import TemplateView
 
-# from .views import QuestionnaireView
 from .views import QuestionnaireView
 
 urlpatterns = [
@@ -10,11 +9,8 @@ urlpatterns = [
         TemplateView.as_view(template_name="questionnaire/instructions.html"),
         name="instructions",
     ),
-    # path("question/", QuestionnaireView.as_view(), name="questionnaire"),
-    # path("question/<int:pk>/", CreateAnswerView.as_view(), name="answer-add"),
-    # ANTOINE
     path(
-        "question/<int:questionaire_pk>/<int:question>",
+        "question/<int:questionnaire_pk>/<int:question>",
         QuestionnaireView.as_view(),
         name="questionnaire",
     ),

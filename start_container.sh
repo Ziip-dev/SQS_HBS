465075f7a101a78851f7f0205e3909f67ea6008c
Ziip <paul.dulaud.2022@utt.fr>
2023-10-03 15:27:11 +0200

    Update start-container.sh script

--- start_container.sh
+++ start_container.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-app="sqshbs"
+app_webserver="SQS_HBS-webserver"
+app_celery="SQS_HBS-celery"
 tag="0.1.2"
 
-# build latest image version
-docker build --file webserver.Dockerfile --pull --rm -t ${app}:${tag} "."
+# Build latest webserver image
+docker build --file webserver.Dockerfile --pull --rm -t ${app_webserver}:${tag} "."
+#
+# Build latest celery image
+docker build --file celery.Dockerfile    --pull --rm -t ${app_celery}:${tag}    "."
 
-# run container
-docker run --name=${app} --rm -d -p 80:7000 \
-    --env-file=.env \
-    ${app}:${tag}
+# Run webserver container
+docker run --name=${app_webserver} --rm -d -p 80:7000 --env-file=.env ${app_webserver}:${tag}
+
+# Run celery container
+docker run --name=${app_celery} --rm -d -p 81:7000 --env-file=.env ${app_celery}:${tag}
